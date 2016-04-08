c0f9ae09f4f27592eeb8cba1b9d6213edec43eb9
Juan Barreiro <jbarreiro@zintro.net>
2016-04-08 15:38:29 -0300

    adding permissions

--- routes/cruds/crud_permissions.js
+++ routes/cruds/crud_permissions.js
@@ -1,11 +1,12 @@
 module.exports = function() {
-    return {
-        shift       : 1000,
-        box         : 1000,
-        accounting  : 1000,
-        exercise    : 750,
-        weight      : 1,
-        category    : 750,
-        wod_week    : 750
-    };
+	return {
+		shift			: 1000,
+		box				: 1000,
+		accounting		: 1000,
+		exercise		: 750,
+		weight			: 1,
+		category		: 750,
+		wod_week		: 750,
+		class_module	: 750
+	};
 };
